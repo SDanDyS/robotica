4af7474acc1f8ea974ea9391b1bfb06b904fef71
SDanDyS <gameking23@hotmail.com>
2022-05-13 12:50:23 +0200

    stash

--- vision/robotVision.py
+++ vision/robotVision.py
@@ -21,6 +21,10 @@ class robotVision(Thread):
         if not self.cap.isOpened():
             print("Cannot open camera")
             exit()
+
+        self.screenWidth  = self.cap.get(cv.CAP_PROP_FRAME_WIDTH)   # float `width`
+        self.screenHeight = self.cap.get(cv.CAP_PROP_FRAME_HEIGHT)  # float `height`
+
         while True:
             # Capture frame-by-frame
             self.ret, self.frame = self.cap.read()
@@ -30,20 +34,12 @@ class robotVision(Thread):
                 print("Can't receive frame (stream end?). Exiting ...")
                 break
 
-            self.screenWidth  = self.cap.get(cv.CAP_PROP_FRAME_WIDTH)   # float `width`
-            self.screenHeight = self.cap.get(cv.CAP_PROP_FRAME_HEIGHT)  # float `height`
-
-            gray = cv.cvtColor(self.frame, cv.COLOR_BGR2GRAY)
-            ret,thresh = cv.threshold(gray,127,255,0)
-  
-            #57 breedte
-            #50 afstand
-            #640 pixel width
-            # self.centroid(thresh)
+            cv.circle(self.frame, (int(self.screenWidth / 2), int(self.screenHeight / 2)), 5, (255, 255, 255), -1)
 
             blur = cv.GaussianBlur(self.frame, (37, 37), 0)
             self.hsv = cv.cvtColor(blur, cv.COLOR_BGR2HSV)
-            self.getFocalLength(self.screenWidth, 50, 57)
+            #distance and width
+            self.getFocalLength(self.screenWidth, 20, 21)
 
             if (self.FLAG == 1):
                 self.detectCookie()
@@ -59,8 +55,8 @@ class robotVision(Thread):
     def detectMovingObject(self):
         # define range of blue color in HSV
         #[[128, 255, 255], [90, 50, 70]]
-        lower_blue = np.array([110, 50, 50])
-        upper_blue = np.array([130, 255, 255])
+        lower_blue = np.array([90, 50, 50])
+        upper_blue = np.array([128, 255, 255])
 
         # Threshold the HSV image to get only blue colors
         mask = cv.inRange(self.hsv, lower_blue, upper_blue)
@@ -70,11 +66,13 @@ class robotVision(Thread):
             # return the biggest contourArea and determine centroid
             blue_area = max(bluecnts, key=cv.contourArea)
             self.centroid(blue_area)
+            distance = self.getDistance(self.cx(blue_area), self.screenWidth)
 
             (xg, yg, wg, hg) = cv.boundingRect(blue_area)
             cv.rectangle(self.frame, (xg, yg), (xg + wg, yg + hg), (0, 255, 0), 2)
-            self.widthToCm(self.cx(blue_area), 20, self.focalLength)
-            cv.line(self.frame, (int(0), int(self.screenHeight / 2)), (int(self.cx(blue_area)), int(self.cy(blue_area))), (0, 255, 0), 2)
+            if (distance != 0):
+                self.widthToCm(self.cx(blue_area), distance, self.focalLength)
+                cv.line(self.frame, (int(0), int(self.screenHeight / 2)), (int(self.cx(blue_area)), int(self.cy(blue_area))), (0, 255, 0), 2)
             # cv.putText(self.frame, "Area detected...", (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv.LINE_4)
 
     def snapshot(self):
@@ -126,21 +124,19 @@ class robotVision(Thread):
         return cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[-2]
 
     # Get the width of the object in pixels
-    # def getPixelWidth(self, img):
+    def getPixelWidth(self, img):
     #     # define range of blue color in HSV
-    #     lower_blue = np.array([110, 50, 50])
-    #     upper_blue = np.array([130, 255, 255])
+        lower_blue = np.array([90, 50, 50])
+        upper_blue = np.array([128, 255, 255])
 
-    #     # Threshold the HSV image to get only blue colors
-    #     mask = cv.inRange(img, lower_blue, upper_blue)
+        # Threshold the HSV image to get only blue colors
+        mask = cv.inRange(img, lower_blue, upper_blue)
 
-    #     bluecnts = self.findContours(mask.copy())
-    #     if (len(bluecnts) > 0):
-    #         blue_area = max(bluecnts, key=cv.contourArea)
+        bluecnts = self.findContours(mask)
+        if (len(bluecnts) > 0):
+            blue_area = max(bluecnts, key=cv.contourArea)
 
-
-
-    #     return cv.minAreaRect(blue_area)
+        return cv.minAreaRect(blue_area)
 
     # Calculate the focal length of the camera
     def getFocalLength(self, pixelWidth, distance, width):
@@ -148,13 +144,17 @@ class robotVision(Thread):
         print("focalLength: " + str(self.focalLength))
 
     # Calculate the distance to an object
-    def getDistance(self, pixelWidth, width=WIDTH_OBJECT):
+    def getDistance(self, pixelWidth, width, focal=False):
+        if (pixelWidth == 0):
+            return 0
         if self.focalLength:
             return (width * self.focalLength) / pixelWidth
+        elif (focal):
+            return (width * focal) / pixelWidth
         else:
             raise ValueError("Focal length was not calculated")
 
-    def widthToCm(self, p, d, f):
-        w = (p * d) / f
-        print("Width in cm: " + str(w))
+    def widthToCm(self, pixel, distance, focal):
+        w = (pixel * distance) / focal
+        print("CM: " + str(w))
         return w
