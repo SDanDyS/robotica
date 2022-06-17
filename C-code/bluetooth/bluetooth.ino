c5eaf0e6882dae6d40b8a52ec90b9ce0885a26ad
Robot <test@example.com>
2022-06-17 11:53:24 +0200

    Controller bluetooth code

--- C-code/bluetooth/bluetooth.ino
+++ C-code/bluetooth/bluetooth.ino
@@ -5,29 +5,35 @@
 #include <Adafruit_SSD1327.h>
 #include <Adafruit_GFX.h>
 
-DynamicJsonBuffer jBuffer;
-JsonObject& root = jBuffer.createObject();
-
 const int Battery_voltage = 25;
-const int LED_Drive = 16;
+
+//LEDs
+const int LED_Drive = 4;
 const int LED_Gripper = 17;
+const int LED_RJ = 5;
+const int LED_RJ2 = 18;
 
 //Push Buttons
-const int KnopPush1 = 4;
+const int KnopPush1 = 2;
 const int KnopPush2 = 15;
 
 int KnopPush1_state = 0;
+int KnopPush1_state_old = 0;
 int KnopPush2_state = 0;
+int KnopPush2_state_old = 0;
 
 //Right
-const int Rjoystickx = 34;
-const int Rjoysticky = 35;
+const int Rjoystickx = 35;
+const int Rjoysticky = 32;
 
-const int Rpush = 14;
-int Rpush_state =0; 
+const int Rpush = 34;
+int Rpush_state = 0; 
 
 int Rjoyvaluex = 0;
 int Rjoyvaluey = 0;
+
+int Rstate = 0;
+int Rknop_oud = 0;
 
 String RXstr ="RX";
 char RXchar [7];
@@ -36,7 +42,7 @@ String RYstr ="RY";
 char RYchar [7];
 //-------------------------------------
 //left
-const int Ljoystickx = 32;
+const int Ljoystickx = 14;
 const int Ljoysticky = 33;
 
 const int Lpush = 12;
@@ -73,7 +79,6 @@ Adafruit_SSD1327 display(128, 128, &Wire, OLED_RESET, 1000000);
 BluetoothSerial SerialBT;
 
 //Receive messages
-
 char incomingChar;
 String message = "";
 
@@ -96,41 +101,93 @@ void setup() {
    pinMode(KnopPush2, INPUT);
    pinMode(LED_Drive, OUTPUT);
    pinMode(LED_Gripper, OUTPUT);
+   pinMode(LED_RJ, OUTPUT);
+   pinMode(LED_RJ2, OUTPUT);
+   pinMode(Battery_voltage, INPUT);
 }
 
 void loop() {
- 
+  aStringObject = LYstr, LXstr, RYstr, RXstr; 
+  
+  DynamicJsonBuffer jBuffer;
+  JsonObject& root = jBuffer.createObject();
+  
   unsigned long currentMillis = millis();
   
-  //push buttons
+    //push buttons
     Rpush_state = digitalRead(Rpush);
     Lpush_state = digitalRead(Lpush);
     KnopPush1_state = digitalRead(KnopPush1);
     KnopPush2_state = digitalRead(KnopPush2);
     
-    if(KnopPush1_state == HIGH){
-      Serial.println("knop1");
-    }
-    if(KnopPush2_state == HIGH){
-      Serial.println("knop2");
-    }
+    //Druk knop 1
+    /*if (KnopPush1_state_old != KnopPush1_state) {
+        KnopPush1_state_old = KnopPush1_state;
+        if(KnopPush1_state == HIGH){
+           switch(KnopPush1_state) {
+             case 0:
+                KnopPush1_state = 1;
+                break; 
+           
+             case 1:
+                KnopPush1_state = 0;
+                break; 
+          }
+        }      
+     }*/ 
 
+    if (KnopPush2_state_old != KnopPush2_state) {
+        KnopPush2_state_old = KnopPush2_state;
+        if(KnopPush2_state == HIGH){
+          Lstate = 0;
+          Rstate = 0;
+        }      
+     }  
+    //Druk knop 2
+    /*if (KnopPush2_state_old != KnopPush2_state) {
+        KnopPush2_state_old = KnopPush2_state;
+        if(KnopPush2_state == HIGH){
+          Lstate = 0;
+          Rstate = 0;
+           switch(KnopPush2_state) {
+             case 0:
+                KnopPush2_state = 1;
+                break; 
+           
+             case 1:
+                KnopPush2_state = 0;
+                break; 
+          }
+        }      
+     }*/  
+
+    //Left joystick button
     if(Lpush_state != Lknop_oud){
       Lknop_oud = Lpush_state;
       
       if(Lpush_state == HIGH){
-        if(Lstate == 0){
-          Lstate = 1;
-        }
-        else if(Lstate == 1){
-          Lstate = 2;
-        }
-        else if(Lstate == 2){
-          Lstate = 1;
+        Rstate = 0;
+         switch(Lstate) {
+           case 0:
+              Lstate = 1;
+              break; /* optional */
+         
+           case 1:
+              Lstate = 2;
+              break; /* optional */
+          
+           /* you can have any number of case statements */
+           case 2: /* Optional */
+           Lstate = 1;
+           break;
         }
       }     
     }
 
+    if(Lstate == 0){
+      digitalWrite(LED_Drive, LOW);
+      digitalWrite(LED_Gripper, LOW);      
+    }    
     if(Lstate == 1){
       digitalWrite(LED_Drive, HIGH);
       digitalWrite(LED_Gripper, LOW);      
@@ -139,7 +196,52 @@ void loop() {
       digitalWrite(LED_Drive, LOW);
       digitalWrite(LED_Gripper, HIGH);        
     }
-    
+
+    //Right joystick button
+    if(Rpush_state != Rknop_oud){
+      Rknop_oud = Rpush_state;
+      
+      if(Rpush_state == HIGH){
+        Lstate = 0;
+         switch(Rstate) {
+           case 0:
+              Rstate = 1;
+              break; /* optional */
+         
+           case 1:
+              Rstate = 2;
+              break; /* optional */
+          
+           /* you can have any number of case statements */
+           case 2: /* Optional */
+           Rstate = 3;
+           break;
+
+           case 3:
+              Rstate = 1;
+              break; /* optional */
+        }
+      }     
+    }
+    //LED status
+    if(Rstate == 0 and Lstate == 0){
+      digitalWrite(LED_RJ, LOW);
+      digitalWrite(LED_RJ2, LOW);
+      digitalWrite(LED_Drive, LOW);
+      digitalWrite(LED_Gripper, LOW);              
+    }
+    if(Rstate == 1){
+      digitalWrite(LED_RJ, HIGH);
+      digitalWrite(LED_RJ2, LOW);      
+    }
+    if(Rstate == 2){
+      digitalWrite(LED_RJ, LOW);
+      digitalWrite(LED_RJ2, HIGH);        
+    }
+    if(Rstate == 3){
+      digitalWrite(LED_RJ, LOW);
+      digitalWrite(LED_RJ2, LOW);        
+    }    
   //waardes op scherm laten zien en versturen via Bluetooth
   if (currentMillis - previousMillis >= interval){
     previousMillis = currentMillis;
@@ -151,27 +253,93 @@ void loop() {
     //Y waarde Left
     Ljoyvaluey = analogRead(Ljoysticky);
     display.setCursor(3,20);
-    display.print("Y waarde L = ");
-    display.print(Ljoyvaluey);
-  
+    display.print("LY = ");
+    if(Lstate ==2){
+      if(Ljoyvaluey > 4000){
+        display.print("Lift up");
+      }
+      else if(Ljoyvaluey <1){
+        display.print("Lift down");
+      }
+      else {
+        display.print("Lift standby");
+      }
+    }
+    if(Lstate == 1){
+      if(Ljoyvaluey > 4000){
+        display.print("F Forward");
+      }
+      else if(Ljoyvaluey <1){
+        display.print("F Backwards");
+      }
+      else if(Ljoyvaluey > 0 and Ljoyvaluey <1910){
+        display.print("S Backwards");
+      }
+      else if(Ljoyvaluey > 1935 and Ljoyvaluey <4001){
+        display.print("S Fowards");
+      }  
+      else {
+        display.print("Hold");
+      }      
+    }
+    if(Lstate == 0){
+      display.print("standby");
+    }
+    
     //X waarde Left
     Ljoyvaluex = analogRead(Ljoystickx);
     display.setCursor(3,30);
-    display.print("x waarde L = ");
-    display.print(Ljoyvaluex);
+    display.print("LX = Not Used");    
       
-    
     //Y waarde Right
     Rjoyvaluey = analogRead(Rjoysticky);
     display.setCursor(3,40);
-    display.print("Y waarde R = ");
-    display.print(Rjoyvaluey);
+    display.print("RY = ");
+    if(Lstate == 1){
+      if(Rjoyvaluey > 4000){
+        display.print("F Forward");
+      }
+      else if(Rjoyvaluey <1){
+        display.print("F Backwards");
+      }
+      else if(Rjoyvaluey > 0 and Rjoyvaluey <1917){
+        display.print("S Backwards");
+      }
+      else if(Rjoyvaluey > 1990 and Rjoyvaluey <4001){
+        display.print("S Fowards");
+      }  
+      else {
+        display.print("Hold");
+      }      
+    }
+    if(Lstate == 0){
+      display.print("standby");
+    }
+    if(Lstate == 2){
+      display.print("Not Used");
+    }
     
     //X waarde Right
     Rjoyvaluex = analogRead(Rjoystickx);
     display.setCursor(3,50);
-    display.print("x waarde R = ");
-    display.print(Rjoyvaluex);
+    display.print("RX = ");
+    if(Lstate == 2){
+      if(Rjoyvaluex > 4000){
+        display.print("Gripper close");
+      }
+      else if(Rjoyvaluex <1){
+        display.print("Gripper open ");
+      }
+      else {
+        display.print("Gripper standby");
+      }
+    }
+    if(Lstate == 0){
+      display.print("standby");
+    }
+    if(Lstate == 1){
+      display.print("Not Used");
+    }
     
     LYstr = String(Ljoyvaluey);
     LXstr = String(Ljoyvaluex);
@@ -182,20 +350,56 @@ void loop() {
     root["LX"] = String(Ljoyvaluex);
     root["RY"] = String(Rjoyvaluey);
     root["RX"] = String(Rjoyvaluex);
+    root["driveOrGrip"] = String(Lstate);
+    root["flag"] = String(Rstate);
+//    root["dance"] = String(KnopPush2_state);
+    
 
     String json;
     root.printTo(json);
     Serial.println(json);//THIS LINE SHOULD BE REMOVED ONCE WE INTEGRATE
     SerialBT.println(json);
     
+    //Controller
+    display.setCursor(40,10);
+    display.print("Controller");
+    display.setCursor(3,70);
+    display.print("Modus: ");
+
+    //Modus weergeven
+    if (Lstate == 0 and Rstate ==0 ){
+      display.print("Standby");
+    }
+    else if (Lstate == 1){
+       display.print("Drive");
+    }
+    else if (Lstate == 2){
+      display.print("Gripper");
+    }
+    else if (Lstate == 0 and Rstate == 1){
+      display.print("Vision");
+    }
+    else if (Lstate == 0 and Rstate == 2){
+      display.print("Cookies");
+    }
+    else if (Lstate == 0 and Rstate == 3){
+      display.print("Dancing");
+    }
+
     //Robot
-    display.setCursor(55,10);
+    display.setCursor(55,85);
     display.print("Robot");
+    display.setCursor(3,95);
+    display.print("Battery = ");
+    display.print(" mV");
+    display.setCursor(3,105); 
+    display.print("Gewicht = ");
+    display.print("g");
     
-    //Battery voltage
+    //Battery voltage controller
     display.setCursor(3,60);
     display.print("Battery = ");
-    display.print((float)analogRead(Battery_voltage)*0.001721727234, 2);
+    display.print((float)analogRead(Battery_voltage)*0.001681727234, 2);
     display.print(" V"); 
   }
 
@@ -218,5 +422,6 @@ void loop() {
 void testdrawrect(void) {
   
     display.drawRect(0, 0, 128, 80, SSD1327_WHITE); 
+    display.drawRect(0, 82, 128, 46, SSD1327_WHITE); 
 
 }
